cdebe343a8a8fdab0258ba533551975dbd4df23d
yahwehboy <110900597+yahwehboy@users.noreply.github.com>
2024-01-18 12:00:15 -0800

    Update yahscrape.py

--- yahscrape.py
+++ yahscrape.py
@@ -3,19 +3,32 @@ from bs4 import BeautifulSoup
 
 #sets a default header for our requests
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-}
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    }
 
-#gets the target website using its url
-target = requests.get('https://examplesite.com/', headers=headers ).content
+#gets user inputs 
+url = input("Enter the targets url 'https://example.com/': ")
+selector = input("Enter the CSS selector '.bdt-ep-advanced-icon-box-content': ")
 
-#parse the website content using BeautifulSoup
-soup = BeautifulSoup(target, 'html.parser')
 
-#gets the specific content using CSS selectors
-results= soup.select('.bdt-ep-advanced-icon-box-content')
+#define the function
+def yahscrape(x, y):
+    
+    #gets the target website using its url
+    target = requests.get(url, headers=headers).content
+    
+    
+        
+    #parse the website content using BeautifulSoup
+    soup = BeautifulSoup(target, 'html.parser')
 
-#saves the results to txt/csv file
-with open("result.txt", "w", encoding="utf-8") as output:
-    for result in results:
-        output.write(f"{result.text}\n")
+    #gets the specific content using CSS selectors
+    results= soup.select(selector)
+
+    #saves the results to txt/csv file
+    with open("result.txt", "w", encoding="utf-8") as output:
+        for result in results:
+            output.write(f"{result.text}\n")
+
+#call the function
+yahscrape(url, selector)
